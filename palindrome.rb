545afd779e4c7bf79dcb6aabd569c6563f128277
Dennis Nderitu <dknderitu@gmail.com>
2015-07-23 14:14:14 +0300

    Update palindrome.rb

--- palindrome.rb
+++ palindrome.rb
@@ -2,11 +2,12 @@
 
 print "please enter a word:"
 
-user_input = gets
+user_input = gets.chomp
 
 reversed_word = user_input.reversed
 
-if (user_input == reversed_word)puts true
+if (user_input == reversed_word)
+      puts "true"
 else
-puts false
+       puts "false"
 end
